b4c39e0dba5a242a2d2e6ff0d142e82676447bf5
mjy <diapriid@gmail.com>
2022-01-20 13:41:22 -0600

    Stub gem

---
+++ lib/colrapi.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "colrapi/version"
+
+module Colrapi
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/colrapi/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Colrapi
+  VERSION = "0.1.0"
+end
