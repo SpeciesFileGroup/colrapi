af56a65c18c11de7598c6bd364113f096df971f9
Geoff Ower <gdower@illinois.edu>
2022-01-27 16:58:09 -0600

    Expanded nameusage params

--- test/nameusage_test.rb
+++ test/nameusage_test.rb
@@ -11,7 +11,22 @@ class TestNameusage < Test::Unit::TestCase
       assert_equal(true, res['result'].length > 0)
       assert_equal(Array, res['result'].class)
     end
-    assert_equal(200, res[0].status)
+  end
+
+  def test_nameusage_offset_limit
+    VCR.use_cassette("test_nameusage_offset_limit") do
+      res = Colrapi.nameusage(q: @name, offset: 2, limit: 1)
+      assert_equal(2, res['offset'])
+      assert_equal(1, res['limit'])
+
+    end
+  end
+
+  def test_nameusage_rank
+    VCR.use_cassette("test_nameusage_rank") do
+      res = Colrapi.nameusage(q: 'Atta', rank: 'genus')
+      assert_equal('genus', res['result'][0]['classification'].last['rank'])
+    end
   end
 
 end
